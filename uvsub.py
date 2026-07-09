6f50b898be069a8810d77d1d796a63d418fb9f55
Will George <wgeorge@whatsit.astro.wesleyan.edu>
2026-07-09 16:21:22 -0400

    uvsub casa code

---
+++ uvsub.py
@@ -0,0 +1,7 @@
+top_dir = '/Volumes/disks/Will_George/Data/'
+targ_name = 'MHO6'
+tag = 'B4_hi'
+
+vis = top_dir + targ_name + '/' + targ_name + '_vis_B4_hi_copyD.ms'
+
+uvsub(vis=vis, reverse=False)
