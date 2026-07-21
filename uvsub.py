3db66c515cf7848514a713c412d339b0d144e654
Will <wgeorge@wesleyan.edu>
2026-07-21 11:47:43 -0400

    Update uvsub.py

--- uvsub.py
+++ uvsub.py
@@ -1,7 +1,15 @@
-top_dir = '/Volumes/disks/Will_George/Data/'
-targ_name = 'MHO6'
+from casatasks import uvsub, tclean
+
+topdir = '/Volumes/disks/Will_George/Data/KPNO10'
 tag = 'B4_hi'
+vis_ms = topdir + '/' + tag + '/' + 'selfcal_2026-05-19' + '/' + 'KPNO10_B4_hi.contap1.ms'
+model_image = topdir + '/residuals'
 
-vis = top_dir + targ_name + '/' + targ_name + '_vis_B4_hi_copyD.ms'
+uvsub(vis=vis_ms)
 
-uvsub(vis=vis, reverse=False)
+tclean(vis=vis_ms,
+       imagename=model_image,
+       datacolumn='corrected',
+       imsize=[250,250],
+       cell='0.025arcsec',
+       niter=0)
